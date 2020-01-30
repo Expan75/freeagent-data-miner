819a2e449b98442af144fe13f15ae690f8a9d824
stigsvensson <erik.hakansson96@gmail.com>
2020-01-30 18:08:08 +0000

    dataflow working for reviewsio but only getting exactly multiples of 20 results. Looks like a bug on the site's part

--- reviewsio-scraper.py
+++ reviewsio-scraper.py
@@ -9,6 +9,8 @@ from time import sleep
 # installed Modules
 from bs4 import BeautifulSoup
 import pandas as pd
+from requests import get
+
 
 # Relative Imports (util functions)
 from utils import getPageRes, getSoup, saveObjects
@@ -16,55 +18,16 @@ from utils import getPageRes, getSoup, saveObjects
 # CONSTS
 BASE_URL = 'https://www.reviews.co.uk/company-reviews/store/freeagent'
 # example of page 2
-
 # # https://www.reviews.co.uk/company-reviews/store/freeagent/1
-
-def getRatingDivs(soup):
-    """ takes in a soup object and returns all the reviews' rating divs """
-    return soup.find_all('div', class_='Review__overallStars__stars')
-
-
-def numerifyStarStringList(starStringList):
-    """ Helper method for converting star icons into a numerical measure """
-
-    rating = 0
-    for starString in starStringList:
-        # handle full stars
-        if starString == 'icon-full-star-01':
-            rating += 1
-        else:
-            continue
-    return rating
-
-
-def getRatings(ratingDivs):
-    """ extracts and return the numerical rating of every review div; returns a list of int:s """
-
-    ratingElements = list(map(lambda ratingDiv: ratingDiv.find_all('i'), ratingDivs))
-    
-    # convert list of ratingElements to their respective class texts
-    textifiedRatingsElements = list(map(lambda: [ itag['class'][1] for itag in x if itag ], ratingElements))
-    
-    # convert that into a simple numerical measure
-    numericalRatings = list(map(lambda x: numerifyStarStringList(x), textifiedRatingsElements))
-
-    return numericalRatings
 
 
 def getReviewText(soup):
-
+    """ extracts all the script tags within a soup object """
     # Text is actually wrapped inside a script tag as JSON data
     data = soup.find_all('script', 'application/ld+json')
-    print(data)
+
     return data
 
-res = getPageRes(BASE_URL)
-soup = getSoup(res)
-
-# ratingDivs = getRatingDivs(soup)
-# ratingTexts = getRatings(ratingDivs)
-
-import json
 
 def extractScriptInnards(soup, index):
     """ takes in a soup object and returns the innards of the indexed script """
@@ -77,7 +40,7 @@ def extractScriptInnards(soup, index):
     return loadedJson['review']
 
 
-def extractReviewData(review):
+def extractReviewData(reviewObject):
     """ takes in a review object and transforms it into one of the following dict: 
         'reviewText' : str,
         'reviewValue' : str,
@@ -85,23 +48,47 @@ def extractReviewData(review):
     """
 
     # Extract rel. info
-    reviewText = review['reviewBody']
-    reviewValue = review['reviewRating']['ratingValue']
-    datePublished = review['datePublished']
+    reviewText = reviewObject['reviewBody']
+    reviewValue = reviewObject['reviewRating']['ratingValue']
+    datePublished = reviewObject['datePublished']
 
     return {
-        "reviewText": reviewText,
         "reviewValue": reviewValue,
+        "reviewText": reviewText,
         "datePublished": datePublished
     }
 
 
+def transformRawReviewData(rawReviewData):
+    """ transforms rawReviewsData into a list of object contianing only rel. info """
+    return list(map(lambda rawObject: extractReviewData(rawObject), rawReviewData))
 
 
-rawScriptInnard = extractScriptInnards(soup, 8)
+def initDataFlow(maxPageLimit):
+    """ Continually scrapes review objects and saves them to file until hitting 404 """
+    
+    loggedResults = 0
 
-print(extractReviewData(rawScriptInnard[0]))
+    for resultPageIndex in range(0, maxPageLimit):
+        # for n == 1; basic URL is fine
+        constructedURL = BASE_URL
+        if resultPageIndex >= 1:
+            constructedURL = constructedURL + '/%s' %resultPageIndex
 
+        # get page soup & handle potential 404s
+        res = getPageRes(constructedURL)
+        if str(res.status_code) == '404':
+            break
 
-# sorted_string = json.dumps(loadedJson, indent=4, sort_keys=False)
-# print(sorted_string)
+        soup = getSoup(res)
+
+        # TODO: find a better way than to hardcode index
+        rawReviewData = extractScriptInnards(soup, 8)
+        data = transformRawReviewData(rawReviewData)
+
+        # save objects and log it
+        loggedResults += saveObjects(data, "data/reviews-io-reviews.csv")
+
+    return "Ended gracefully with %s saved." %loggedResults
+
+print(initDataFlow(99))
